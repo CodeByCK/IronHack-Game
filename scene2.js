b968d29ff8b2384dfadecb14d0cf74c0600744a2
Christian <christiankhawam@outlook.com>
2019-04-14 12:28:29 -0400

    added more vehicles and fixed speeds

--- scene2.js
+++ scene2.js
@@ -1,6 +1,6 @@
 
 // cars / objects / character
-let car;
+let car1;
 let car2;
 let car3;
 let car4;
@@ -8,6 +8,49 @@ let car5;
 let car6;
 let car7;
 let car8;
+let car9;
+let car10;
+let car11;
+let car12;
+let car13;
+let car14;
+let car15;
+let car16;
+let car17;
+let car18;
+let car19;
+let car20;
+let car30;
+let car31;
+let car32;
+let car33;
+let car34;
+let car35;
+let car36;
+let car37;
+let car38;
+let car39;
+let car40;
+let car41;
+let car42;
+let car43;
+let car44;
+let car45;
+let car46;
+let car47;
+let car48;
+let car49;
+let car50;
+let car51;
+let car52;
+let car53;
+let car54;
+let car55;
+let car56;
+let car57;
+let car58;
+let car59;
+let car60;
 let player;
 let playerSpeed = 5;
 let zone;
@@ -16,8 +59,8 @@ let scoreText;
 let score = 0;
 let gameOverText;
 //random number for car speed
-let min = 1
-let max = 3
+let min = 8
+let max = 20
 let randomNum = Math.floor(Math.random() * (max - min) + min)
 let randomNum1 = Math.floor(Math.random() * (max - min) + min)
 let randomNum2 = Math.floor(Math.random() * (max - min) + min)
@@ -26,6 +69,14 @@ let randomNum4 = Math.floor(Math.random() * (max - min) + min)
 let randomNum5 = Math.floor(Math.random() * (max - min) + min)
 let randomNum6 = Math.floor(Math.random() * (max - min) + min)
 let randomNum7 = Math.floor(Math.random() * (max - min) + min)
+let randomNum8 = Math.floor(Math.random() * (max - min) + min)
+let randomNum9 = Math.floor(Math.random() * (max - min) + min)
+let randomNum10 = Math.floor(Math.random() * (max - min) + min)
+let randomNum11 = Math.floor(Math.random() * (max - min) + min)
+let randomNum12 = Math.floor(Math.random() * (max - min) + min)
+let randomNum13 = Math.floor(Math.random() * (max - min) + min)
+let randomNum14 = Math.floor(Math.random() * (max - min) + min)
+let randomNum15 = Math.floor(Math.random() * (max - min) + min)
 
 
 class scene2 extends Phaser.Scene {
@@ -36,6 +87,8 @@ class scene2 extends Phaser.Scene {
 
     preload() {//everything you need to load here
         this.load.image('background', './assets/bg1.png')
+        this.load.image('car10', './assets/car4.png')
+        this.load.image('car9', './assets/car.png')
         this.load.image('car8', './assets/car4.png')
         this.load.image('car7', './assets/car2.png')
         this.load.image('car6', './assets/car.png')
@@ -43,7 +96,7 @@ class scene2 extends Phaser.Scene {
         this.load.image('car4', './assets/car4.png')
         this.load.image('car3', './assets/car2.png')
         this.load.image('car2', './assets/car4.png');
-        this.load.image('car', './assets/car.png');
+        this.load.image('car1', './assets/car.png');
         this.load.spritesheet('player', './assets/chicken.png', { frameWidth: 50, frameHeight: 40 })
         this.load.audio('over', './assets/GAMEOVER.wav')
         this.load.audio('jump', 'http://freesound.org/data/previews/456/456374_9498993-lq.mp3');
@@ -60,6 +113,8 @@ class scene2 extends Phaser.Scene {
         // this.image = this.add.image(400, -1900, 'background');
         this.physics.world.setBounds(0, 0, 800, 5000);
         //make object move
+        car10 = this.add.sprite(-200, 723, 'car10')//lane11
+        car9 = this.add.sprite(-200, 673, 'car9')//lane10
         car8 = this.add.sprite(-200, 623, 'car8')//lane9
         car7 = this.add.sprite(-210, 573, 'car7');//lane8
         car6 = this.add.sprite(-220, 473, 'car6');//lane7
@@ -67,7 +122,7 @@ class scene2 extends Phaser.Scene {
         car4 = this.add.sprite(-200, 373, 'car4')//lane5
         car3 = this.add.sprite(-210, 273, 'car3')//lane4
         car2 = this.add.sprite(-220, 223, 'car2');//lane3
-        car = this.add.sprite(-190, 123, 'car');//lane2
+        car1 = this.add.sprite(-190, 123, 'car1');//lane2
         player = this.physics.add.sprite(400, 75, 'player');//lane1
         player.setCollideWorldBounds(true)
 
@@ -135,12 +190,12 @@ class scene2 extends Phaser.Scene {
     }
     update(delta) { //animate
         // move cars
-        car.x -= randomNum;
-        if (car.x < -100) {
-            car.x = 950;
+        car1.x -= randomNum;
+        if (car1.x < -100) {
+            car1.x = 950;
             randomNum = Math.floor(Math.random() * (max - min) + min)
         }
-        if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car.getBounds())) {
+        if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car1.getBounds())) {
             this.gameOver();
 
         }
@@ -172,41 +227,60 @@ class scene2 extends Phaser.Scene {
             this.gameOver();//collision
         }
 
-        car5.x += randomNum;
-        car5.flipX = true;
-        if (car5.x > 800) {
-            car5.x = -250;
+        car5.x -= randomNum4;
+        if (car5.x < -100) {
+            car5.x = 950;
             randomNum4 = Math.floor(Math.random() * (max - min) + min)
+
         }
         if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car5.getBounds())) {
             this.gameOver();//collision
         }
-        car6.x -= randomNum1;
-        if (car6.x < -100) {
-            car6.x = 950;
+
+        car6.x += randomNum5;
+        car6.flipX = true;
+        if (car6.x > 800) {
+            car6.x = -145;
             randomNum5 = Math.floor(Math.random() * (max - min) + min)
+            if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car6.getBounds())) {
+                this.gameOver();//collision
+            }
         }
-        if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car6.getBounds())) {
-            this.gameOver();//collision
-        }
-        car7.x += randomNum2;
-        car7.flipX = true;
-        if (car7.x > 800) {
-            car7.x = -145;
+        car7.x -= randomNum6;
+        if (car7.x < -100) {
+            car7.x = 950;
             randomNum6 = Math.floor(Math.random() * (max - min) + min)
 
         }
         if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car7.getBounds())) {
             this.gameOver();//collision
         }
-        car8.x -= randomNum3;
-        if (car8.x < -100) {
-            car8.x = 950;
+        car8.x += randomNum7;
+        car8.flipX = true;
+        if (car8.x > 800) {
+            car8.x = -145;
             randomNum7 = Math.floor(Math.random() * (max - min) + min)
+            if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car8.getBounds())) {
+                this.gameOver();//collision
+            }
         }
-        if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car8.getBounds())) {
+        car9.x -= randomNum8;
+        if (car9.x < -100) {
+            car9.x = 950;
+            randomNum8 = Math.floor(Math.random() * (max - min) + min)
 
+        }
+        if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car9.getBounds())) {
             this.gameOver();//collision
+        }
+        car10.x += randomNum9;
+        car10.flipX = true;
+        if (car10.x > 800) {
+            car10.x = -145;
+            randomNum9 = Math.floor(Math.random() * (max - min) + min)
+            if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), car10.getBounds())) {
+                this.gameOver();//collision
+            }
         }
     };
 
@@ -239,7 +313,7 @@ class scene2 extends Phaser.Scene {
         this.stars = this.physics.add.group({
             key: 'star',
             repeat: 100,
-            setXY: { x: 400, y: 30, stepY: 50 }
+            setXY: { x: 400, y: 123, stepY: 50 }
         });
 
         this.physics.add.overlap(player, this.stars, this.collectStar, null, this);
@@ -247,8 +321,8 @@ class scene2 extends Phaser.Scene {
 
     collectStar(player, star) {
         star.disableBody(true, true)
-        score += 10;
-        this.scoreText.setText('Score: ' + score);
+        score += 1;
+        this.scoreText.setText('Lanes: ' + score);
     }
 
 }
